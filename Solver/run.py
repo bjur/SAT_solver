25a2c109c662d2d77df192851a30756a545e13b2
bjur <bor.juros@gmail.com>
2016-03-22 15:34:29 +0100

    Added files via upload

--- Solver/run.py
+++ Solver/run.py
@@ -6,7 +6,6 @@ input_file = readDimacs(sys.argv[1])
 
 print("Reading *.cnf file")
 print("Finding solution")
-#sol=solve(And(1,Not(1)),[])
 sol = solve(input_file,[])
 if sol == False:
     print "No solution found"
