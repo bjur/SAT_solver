8ba853be3582024bd66d162bf0ee084a22989972
bjur <bor.juros@gmail.com>
2016-03-31 00:23:01 +0200

    Added files via upload

---
+++ run.py
@@ -0,0 +1,19 @@
+from sat import *
+import sys
+from read_file import *
+
+input_file = readDimacs(sys.argv[1])
+
+print("Reading *.cnf file")
+print("Finding solution")
+sol = solve(input_file,[])
+if sol == False:
+    print "No solution found"
+else:
+    bs = buildSoultion(sol)
+    #h=input_file.evaluate(bs)
+    h=True;
+    print "Solution was found: ", h
+    if h:
+        print "Solution: ",bs
+        print "Solution evaluation: ",h
