1eaf6bcf09f36fabb486618faffa2e1db1e26b63
bjur <bor.juros@gmail.com>
2016-04-12 22:48:40 +0200

    Added files via upload

--- Solver/run.py
+++ Solver/run.py
@@ -5,8 +5,11 @@ from read_file import *
 input_file = readDimacs(sys.argv[1])
 sol = solve(input_file,[])
 if sol == False:
-    print -1
+    print "Solution not found"
 else:
     for s in sol:
         if not isinstance(s,Not):
             print int(s.__str__()),
+        else:
+            print -int(s.__str__()[1:]),
+print sol
